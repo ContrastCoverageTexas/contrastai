fae7f02a1c3be43cdee275409363bd541d813316
ContrastCoverageTexas <162303200+ContrastCoverageTexas@users.noreply.github.com>
2024-03-05 22:39:11 -0800

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,26 +1,22 @@
 import requests
 
-WEBHOOK_URL = "https://hooks.slack.com/services/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"
+WEBHOOK_URL= "https://hooks.slack.com/services/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"
 
 def send_to_slack(message: str):
-    """
-    Sends a message to Slack
-    """
-    response = requests.post(
-        WEBHOOK_URL,
-        json={
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": message
-                    }
-                }
-            ]
+  """
+  Sends a message to Slack
+  """
+  return requests.post(
+    WEBHOOK_URL,
+    json={
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": message
+          }
         }
-    )
-    # Log response for debugging
-    print(f"Response code: {response.status_code}")
-    print(f"Response body: {response.text}")
-    return response
+      ]
+    }
+  ) 
