64d8bdd8b0950be6355966bfeb4a489565f0008b
ContrastCoverageTexas <162303200+ContrastCoverageTexas@users.noreply.github.com>
2024-03-05 16:37:16 -0800

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,23 +1,33 @@
 import os
 import requests
 
-WEBHOOK_URL = "https://hooks.slack.com/services/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"
+# Use an environment variable for the webhook URL
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
 
 def send_to_slack(message: str):
-  """
-  Sends a message to Slack
-  """
-  return requests.post(
-    WEBHOOK_URL,
-    json={
-      "blocks": [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": message
-          }
+    """
+    Sends a message to Slack.
+    """
+    if not WEBHOOK_URL:
+        print("SLACK_WEBHOOK_URL environment variable not set.")
+        return None
+
+    response = requests.post(
+        WEBHOOK_URL,
+        json={
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": message
+                    }
+                }
+            ]
         }
-      ]
-    }
-  ) 
+    )
+    try:
+        response.raise_for_status()  # Raises an error for bad responses
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+    return response
