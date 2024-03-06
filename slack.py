71a88110d4aebc069ac9f8728027a1b8dcec5a2d
ContrastCoverageTexas <162303200+ContrastCoverageTexas@users.noreply.github.com>
2024-03-05 23:05:13 -0800

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,22 +1,12 @@
 import requests
 
 def send_to_slack(message: str):
-  """
-  Sends a message to Slack
-  """
-  WEBHOOK_URL= "https://hooks.slack.com/services/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"
-  
-  return requests.post(
-    WEBHOOK_URL,
-    json={
-      "blocks": [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": message
-          }
-        }
-      ]
-    }
-  ) 
+    """
+    Sends a simple text message to Slack
+    """
+    WEBHOOK_URL= "https://hooks.slack.com/services/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"
+    
+    return requests.post(
+      WEBHOOK_URL,
+      json={"text": message}
+    )
