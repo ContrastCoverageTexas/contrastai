5c549af49c6ca21f5f94b9d9924f25f29e577b60
ContrastCoverageTexas <162303200+ContrastCoverageTexas@users.noreply.github.com>
2024-03-05 20:02:35 -0800

    Update my_pdf_lib.py

--- my_pdf_lib.py
+++ my_pdf_lib.py
@@ -9,7 +9,7 @@ import pickle
 from langchain_community.docstore.document import Document
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings.openai import OpenAIEmbeddings
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores.faiss import FAISS
 from pypdf import PdfReader
 import faiss
