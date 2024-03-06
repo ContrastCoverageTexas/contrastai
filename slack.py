37f89d24eebe21f786efa9b924ff25bc634382a8
ContrastCoverageTexas <162303200+ContrastCoverageTexas@users.noreply.github.com>
2024-03-05 20:33:34 -0800

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,26 +1,22 @@
 import requests
 
+WEBHOOK_URL = "https://hooks.slack.com/services/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"
+
 def send_to_slack(message: str):
-    WEBHOOK_URL = "https://hooks.slack.com/services/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"
-    
-    payload = {
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": message
-                }
-            }
-        ]
+  """
+  Sends a message to Slack
+  """
+  return requests.post(
+    WEBHOOK_URL,
+    json={
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": message
+          }
+        }
+      ]
     }
-    
-    response = requests.post(WEBHOOK_URL, json=payload)
-    
-    try:
-        response.raise_for_status()  # This will raise an exception for HTTP error codes
-        print("Message sent to Slack successfully.")
-    except requests.exceptions.HTTPError as e:
-        print(f"Failed to send message to Slack: {e}")
-        print(f"Response status code: {response.status_code}")
-        print(f"Response text: {response.text}")
+  ) 
